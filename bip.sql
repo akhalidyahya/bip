e2f0fc52e2acd3bdd22dd9a79e097048cc8fb70a
RULNJT0PZN1BSFU\Administrator <khalid.yahya9@gmail.com>
2019-03-26 17:59:58 +0700

    db

--- bip.sql
+++ bip.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 11, 2019 at 11:33 AM
+-- Generation Time: Mar 26, 2019 at 11:59 AM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.2.1
 
@@ -25,6 +25,79 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `activities`
+--
+
+CREATE TABLE `activities` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `judul` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `isi` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tanggal` varchar(25) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `businesses_id` int(10) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `activities`
+--
+
+INSERT INTO `activities` (`id`, `judul`, `isi`, `tanggal`, `businesses_id`, `created_at`, `updated_at`) VALUES
+(1, 'First Meeting', 'onclick=\"saveActivity()\"', '18 March 2019 - 22:00', 3, '2019-03-18 08:03:53', '2019-03-18 08:03:53'),
+(5, 'Second Meeting', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '19 March 2019 - 04:30', 3, '2019-03-18 14:30:49', '2019-03-18 14:30:49');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `anggotas`
+--
+
+CREATE TABLE `anggotas` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nama` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `angkatan` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `instansi` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `businesses_id` int(10) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `anggotas`
+--
+
+INSERT INTO `anggotas` (`id`, `nama`, `angkatan`, `instansi`, `businesses_id`, `created_at`, `updated_at`) VALUES
+(1, 'Yahya', '2016', '', 3, '2019-03-17 23:15:28', '2019-03-17 23:15:28'),
+(2, 'Aryo', '2016', '', 3, '2019-03-17 23:15:28', '2019-03-17 23:15:28'),
+(3, 'Syauqi', '2017', '', 3, '2019-03-17 23:15:29', '2019-03-17 23:15:29');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bisnis`
+--
+
+CREATE TABLE `bisnis` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nama` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lokasi` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pendapatan` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `penjelasan` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `foto` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `bisnis`
+--
+
+INSERT INTO `bisnis` (`id`, `nama`, `lokasi`, `pendapatan`, `penjelasan`, `foto`, `created_at`, `updated_at`) VALUES
+(3, 'Web Dev Inc', 'Depok', '10000', 'kumpulan web developer', NULL, '2019-03-17 23:15:28', '2019-03-17 23:15:28');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `migrations`
 --
 
@@ -40,7 +113,13 @@ CREATE TABLE `migrations` (
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2014_10_12_000000_create_users_table', 1),
-(2, '2014_10_12_100000_create_password_resets_table', 1);
+(2, '2014_10_12_100000_create_password_resets_table', 1),
+(3, '2019_03_18_053433_create_bisnis_table', 2),
+(4, '2019_03_18_053649_create_anggotas_table', 3),
+(5, '2019_03_18_053947_add_fk_anggota_bisnis', 4),
+(6, '2019_03_18_060649_add_table_column_bisnis', 5),
+(7, '2019_03_18_141723_create_activities_table', 6),
+(8, '2019_03_19_075417_update_table_anggotas', 7);
 
 -- --------------------------------------------------------
 
@@ -76,11 +155,31 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-(3, 'Admin BIP', 'admin@email.com', NULL, '$2y$10$sJCJjSdfFHDSj8TebHYQUuU5GuCk0Tn5AYI33nGbM5uSjWqPGlIHK', 'ACBcIXn7i2oGitDsQ8lyBIoEb7xWUHSuk9uuWuLwUl1DO1ZBFQ2XkX6buOp2', '2019-02-13 21:04:52', '2019-02-13 21:04:52');
+(3, 'Admin BIP', 'admin@email.com', NULL, '$2y$10$sJCJjSdfFHDSj8TebHYQUuU5GuCk0Tn5AYI33nGbM5uSjWqPGlIHK', 'xrG1DPPXOtwjfCGECxoOyAWY8i2MPbdUK9QtiYOb2lnHOs8dDD9GSJepVjYB', '2019-02-13 21:04:52', '2019-02-13 21:04:52');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `activities`
+--
+ALTER TABLE `activities`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `activities_businesses_id_foreign` (`businesses_id`);
+
+--
+-- Indexes for table `anggotas`
+--
+ALTER TABLE `anggotas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `anggotas_businesses_id_foreign` (`businesses_id`);
+
+--
+-- Indexes for table `bisnis`
+--
+ALTER TABLE `bisnis`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `migrations`
@@ -106,16 +205,50 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `activities`
+--
+ALTER TABLE `activities`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `anggotas`
+--
+ALTER TABLE `anggotas`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `bisnis`
+--
+ALTER TABLE `bisnis`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `activities`
+--
+ALTER TABLE `activities`
+  ADD CONSTRAINT `activities_businesses_id_foreign` FOREIGN KEY (`businesses_id`) REFERENCES `bisnis` (`id`);
+
+--
+-- Constraints for table `anggotas`
+--
+ALTER TABLE `anggotas`
+  ADD CONSTRAINT `anggotas_businesses_id_foreign` FOREIGN KEY (`businesses_id`) REFERENCES `bisnis` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
